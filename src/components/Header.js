7284037e575a27a1e2c22ec7a4b7fb54c39bafa7
meinou <mskozlovskaya@gmail.com>
2018-07-24 17:57:22 -0400

    header beginning

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,15 @@ import "./Header.css";
 
 class Header extends Component {
   render() {
-    return <div className="header" />;
+    return (
+      <div className="header">
+        <span>ESSTEEM JOB REFERRAL CONTESTS</span>
+        <span class="menu">
+          <div />
+          <div>ABOUT</div>
+        </span>
+      </div>
+    );
   }
 }
 
